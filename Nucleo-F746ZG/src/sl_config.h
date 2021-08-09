f194bccfc7d1c46c753c39075c9053d42985df12
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2021-08-08 21:56:18 -0600

    Add source files

---
+++ Nucleo-F746ZG/src/sl_config.h
@@ -0,0 +1,21 @@
+/* Do not modifiy this file.
+ * It was generated using the sl command line tool
+ * Change the settings using the tool then build again using sl
+ */
+
+#ifndef SL_CONFIG_H_
+#define SL_CONFIG_H_
+
+#define SL_CONFIG_VERSION_STRING "0.13"
+#define SL_CONFIG_VERSION_BCD 0x0D
+#define SL_CONFIG_DOCUMENT_ID "XCw1jMjiObLMUDKZFRG2"
+#define SL_CONFIG_TEAM_ID ""
+#define SL_CONFIG_NAME "Nucleo-F746ZG"
+#define SL_CONFIG_TYPE "os"
+#define SL_CONFIG_PERMISSIONS "public"
+#define SL_CONFIG_HARDWARE_ID_STRING "0x00000013"
+
+//Thread stack sizes
+
+#endif
+
---
+++ Nucleo-F767ZI/src/sl_config.h
@@ -0,0 +1,21 @@
+/* Do not modifiy this file.
+ * It was generated using the sl command line tool
+ * Change the settings using the tool then build again using sl
+ */
+
+#ifndef SL_CONFIG_H_
+#define SL_CONFIG_H_
+
+#define SL_CONFIG_VERSION_STRING "0.16"
+#define SL_CONFIG_VERSION_BCD 0x10
+#define SL_CONFIG_DOCUMENT_ID "8fgoFM3GV4Kos65AegZT"
+#define SL_CONFIG_TEAM_ID ""
+#define SL_CONFIG_NAME "Nucleo-F767ZI"
+#define SL_CONFIG_TYPE "os"
+#define SL_CONFIG_PERMISSIONS "public"
+#define SL_CONFIG_HARDWARE_ID_STRING "0x00000014"
+
+//Thread stack sizes
+
+#endif
+
